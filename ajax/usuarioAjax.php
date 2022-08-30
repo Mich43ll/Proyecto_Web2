58f61781bd26f80f93a1c9c916fffbbf84caa109
Mich43ll <mosoriobarahona@gmail.com>
2022-08-29 19:01:23 -0600

    Modelo Agregar Usuario

---
+++ ajax/usuarioAjax.php
@@ -0,0 +1,15 @@
+<?php
+
+    $peticionAjax=true;
+    require_once "../config/APP.php";
+
+    if(){
+    /*----------------- Instancia al controlador ----------------*/
+        require_once "../controladores/usuarioControlador.php";
+        $ins_usuario = new usuarioControlador();
+    }else{
+        session_start(['name' => 'SPM']);
+        session_unset();
+        session_destroy();
+        header("Location: ".SERVERURL."login/");
+    }
